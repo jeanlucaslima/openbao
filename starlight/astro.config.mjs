e7611ba8514e31e3bb8340728097d71025ff613a
Jean Lucas Lima <jeanleonino@gmail.com>
2023-12-09 21:17:04 -0300

    5min test

---
+++ starlight/astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'openbao',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Platform',
+					autogenerate: { directory: 'platform' },
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
+});
